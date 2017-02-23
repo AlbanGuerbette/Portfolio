86f72118960fc474053b7c17d2ba1b96029ebbec
AlbanGuerbette <aguerbette@gmail.com>
2017-02-23 21:01:47 +0100

    Mise a jour des dependences et de gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,40 +1,65 @@
-/* File: gulpfile.js */
-
-// grab our gulp packages
 var gulp = require('gulp'),
-  gutil = require('gulp-util'),
-  sass = require('gulp-sass'),
-  sourcemaps = require('gulp-sourcemaps'),
-  concat = require('gulp-concat');
+  plumber = require('gulp-plumber'),
+  rename = require('gulp-rename');
+var autoprefixer = require('gulp-autoprefixer');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var cleanCSS = require('gulp-clean-css');
+var sourcemaps = require('gulp-sourcemaps');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync');
 
+gulp.task('browser-sync', function () {
+  browserSync({
+    server: {
+      baseDir: "./"
+    }
+  });
+});
 
-var srcScss = 'source/scss/**/*.scss';
-var destCss = 'public/assets/css';
-var srcJs = 'source/js/**/*.js';
-var destJs = 'public/assets/js';
+gulp.task('bs-reload', function () {
+  browserSync.reload();
+});
 
-
-// create a default task and just log a message
-gulp.task('build-css', function () {
-  return gulp.src(srcScss)
-    .pipe(sourcemaps.init()) // Process the original sources
+gulp.task('styles', function () {
+  gulp
+    .src(['source/scss/**/*.scss'])
+    .pipe(plumber({
+      errorHandler: function (error) {
+        console.log(error.message);
+        this.emit('end');
+      }
+    }))
     .pipe(sass())
-    .pipe(sourcemaps.write()) // Add the map to modified source.
-    .pipe(gulp.dest(destCss));
-});
-
-gulp.task('build-js', function () {
-  return gulp.src(srcJs)
+    .pipe(autoprefixer('last 2 versions'))
+    .pipe(gulp.dest('public/assets/css/'))
+    .pipe(rename({suffix: '.min'}))
     .pipe(sourcemaps.init())
-    .pipe(concat('bundle.js'))
-    //only uglify if gulp is ran with '--type production'
-    .pipe(gutil.env.type === 'production' ? uglify() : gutil.noop())
+    .pipe(cleanCSS())
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest(destJs));
+    .pipe(gulp.dest('public/assets/css/'))
+    .pipe(browserSync.reload({stream: true}))
 });
 
+gulp.task('scripts', function () {
+  return gulp
+    .src('source/js/**/*.js')
+    .pipe(plumber({
+      errorHandler: function (error) {
+        console.log(error.message);
+        this.emit('end');
+      }
+    }))
+    .pipe(concat('main.js'))
+    .pipe(gulp.dest('public/assets/js/'))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(uglify())
+    .pipe(gulp.dest('public/assets/js/'))
+    .pipe(browserSync.reload({stream: true}))
+});
 
-gulp.task('watch', function () {
-  gulp.watch(srcScss, ['build-css']);
-  gulp.watch(srcJs, ['build-js']);
+gulp.task('default', ['browser-sync'], function () {
+  gulp.watch("source/scss/**/*.scss", ['styles']);
+  gulp.watch("source/js/**/*.js", ['scripts']);
+  gulp.watch("*.html", ['bs-reload']);
 });
