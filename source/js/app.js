c71217245141bd19c78ec7c277f65c4f9e974dab
AlbanGuerbette <aguerbette@gmail.com>
2017-03-01 05:26:19 +0000

    Added scroll handler

--- source/js/app.js
+++ source/js/app.js
@@ -34,3 +34,16 @@ $('.menu__item').on('click', function () {
     menuToggle();
   }
 });
+
+
+// Scroll handling a[href*=#]
+$('.bottom-arrow__link').on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 200, 'linear');
+});
+
+
+$('.menu__item').on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 300, 'linear');
+});
